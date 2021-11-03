0016f1f920b98e3471e2ab05aebf679afc6ef3ed
Abhishek <yadavabhishek241296@gmail.com>
2021-11-03 12:23:58 +0530

    initial commit

---
+++ src/learningTS.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import './App.css';
+
+
+// let name: string = "Abhishek";
+// let age: number = 24;
+// let isBoolean: boolean = true;
+// let hobbies: string[] = ['coding', 'writing', 'Abhishek']
+
+// // variable containing two types in typescript
+// let age2: string|number;
+// age2 = '24'
+
+// // functions in typescript
+// let printName = (name: string) => {
+//   console.log(name);
+// }
+// printName('Abhishek')
+
+// // function type in typescript 
+// let handleSelect: (name: string) => void;
+// handleSelect = (name) => {
+//   console.log(name);
+// }
+// handleSelect('Aishwarya')
+
+// // tuple in typescript
+// let a: [number, string];
+// a= [24,'Abhishek']
+
+// // object in typescript
+// type Person ={
+//   name: string;
+//   age: number
+// }
+// let person: Person = {
+//   name: "Abhishek",
+//   age: 24
+// }
+
+// // Array of objects in typescript
+// type Person1 ={
+//   name: string;
+//   age?: number
+// }
+
+// let person1: Person1[] =[{
+//   name: 'Abhishek',
+//   age: 24
+// }]
+
+
+
+function learningTs() {
+  return (
+    <div className="App">
+      This is just a demo file for learning typescript.
+    </div>
+  );
+}
+
+export default learningTs;
